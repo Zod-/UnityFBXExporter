5f4bd65c627688b2b8bdcd660c07d1517f9a6288
Julian Hangstörfer <Julian.Hangstoerfer@generic.de>
2017-08-29 11:12:07 +0200

    Extract methods in procedural test for readability

--- Assets/Packages/UnityFBXExporter/Editor/ProceduralTest.cs
+++ Assets/Packages/UnityFBXExporter/Editor/ProceduralTest.cs
@@ -3,18 +3,29 @@ using UnityEditor;
 
 public static class ProceduralTest
 {
+    [MenuItem("Assets/FBX Exporter/Create Object With Procedural Texture", false, 43)]
+    public static void CreateObject()
+    {
+        var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        var mat = new Material(Shader.Find("Standard"))
+        {
+            mainTexture = ProceduralTexture(128)
+        };
+        cube.GetComponent<MeshRenderer>().sharedMaterial = mat;
+    }
 
-	[MenuItem("Assets/FBX Exporter/Create Object With Procedural Texture", false, 43)]
-	public static void CreateObject()
-	{
-		GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-		Texture2D texture = new Texture2D(128, 128);
-		for (int x = 0; x < 128; ++x)
-			for (int y = 0; y < 128; ++y)
-				texture.SetPixel(x, y, (x-64)*(x-64) + (y-64)*(y-64) < 1000 ? Color.white : Color.black);
-		texture.Apply();
-		Material mat = new Material(Shader.Find("Standard"));
-		mat.mainTexture = texture;
-		cube.GetComponent<MeshRenderer>().sharedMaterial = mat;
-	}
+    private static Texture2D ProceduralTexture(int size)
+    {
+        var texture = new Texture2D(size, size);
+        for (var x = 0; x < size; ++x)
+            for (var y = 0; y < size; ++y)
+                texture.SetPixel(x, y, ProceduralPixelColor(x, y));
+        texture.Apply();
+        return texture;
+    }
+
+    private static Color ProceduralPixelColor(int x, int y)
+    {
+        return (x - 64) * (x - 64) + (y - 64) * (y - 64) < 1000 ? Color.white : Color.black;
+    }
 }
