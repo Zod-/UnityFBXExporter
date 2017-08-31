4bd0ebebe8d09afb9bb5a8c0e93fc924c6e1b7ac
Julian Hangstörfer <jhangstoerfer@gmail.com>
2017-08-31 09:40:51 +0200

    Finish model node

--- Assets/Packages/UnityFBXExporter/Nodes/Objects/FbxModelNode.cs
+++ Assets/Packages/UnityFBXExporter/Nodes/Objects/FbxModelNode.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Specialized;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace UnityFBXExporter
 {
@@ -15,7 +14,39 @@ namespace UnityFBXExporter
             Id = FBXExporter.GetRandomFBXId();
             SubClass = gameObject.HasMesh() ? "Mesh" : "Null";
             _gameObject = gameObject;
+            Node("Version", 232);
+            Node("Shading", 'T');
+            Node("Culling", "CullingOff");
+            Property("RotationOrder", "enum", "", "", 4);
+            Property("RotationActive", "bool", "", "", 1);
+            Property("InheritType", "enum", "", "", 1);
+            Property("ScalingMax", "Vector3D", "Vector", "", Vector3.zero);
+            Property("DefaultAttributeIndex", "int", "Integer", "", 0);
+            Property("currentUVSet", "KString", "", "U", "map1");
+            Position();
+            Rotation();
+            Scaling();
         }
 
+        private void Scaling()
+        {
+            var scale = _gameObject.transform.localScale;
+            Property("Lcl Scaling", "Lcl Scaling", "", "A", scale);
+        }
+
+        private void Rotation()
+        {
+            var rot = _gameObject.transform.localEulerAngles;
+            rot.y *= -1;
+            rot.z *= -1;
+            Property("Lcl Rotation", "Lcl Rotation", "", "A+", rot);
+        }
+
+        private void Position()
+        {
+            var pos = _gameObject.transform.localPosition;
+            pos.x *= -1;
+            Property("Lcl Translation", "Lcl Translation", "", "A+", pos);
+        }
     }
 }
