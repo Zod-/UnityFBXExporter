0fbc332199290aa3e35f0379a278919ee53cea82
Julian Hangstörfer <jhangstoerfer@gmail.com>
2017-09-04 23:11:01 +0200

    Explicitly cast serialize collections

--- Assets/Packages/UnityFBXExporter/Writer/FbxValueSerializer.cs
+++ Assets/Packages/UnityFBXExporter/Writer/FbxValueSerializer.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using UnityEngine;
 
 namespace UnityFBXExporter
@@ -7,7 +8,7 @@ namespace UnityFBXExporter
     {
         public static string Serialize(object value)
         {
-            return value.GetType().IsArray ? SerializeCollection((object[])value) : SerializeValue(value);
+            return value.GetType().IsArray ? SerializeCollection(value as Array) : SerializeValue(value);
         }
 
         public static string SerializeValue(object value)
@@ -19,19 +20,27 @@ namespace UnityFBXExporter
             if (value is Color)
             {
                 var color = (Color)value;
-                return SerializeCollection(new object[] { color.r, color.g, color.b });
+                return SerializeCollection(new[] { color.r, color.g, color.b });
             }
             if (value is Vector3)
             {
                 var color = (Vector3)value;
-                return SerializeCollection(new object[] { color.x, color.y, color.z });
+                return SerializeCollection(new[] { color.x, color.y, color.z });
             }
             return value.ToString();
         }
 
-        public static string SerializeCollection(object[] collection)
+        public static string SerializeCollection(Array collection)
         {
-            return string.Join(",", Array.ConvertAll(collection, i => i.ToString()));
+            if (collection is float[])
+            {
+                return string.Join(",", Array.ConvertAll((float[])collection, i => i.ToString(CultureInfo.InvariantCulture)));
+            }
+            if (collection is int[])
+            {
+                return string.Join(",", Array.ConvertAll((int[])collection, i => i.ToString()));
+            }
+            return string.Empty;
         }
     }
 }
