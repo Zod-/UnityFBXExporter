78fc0ce82187140633b179b376a185b3908febc7
Julian Hangstörfer <jhangstoerfer@gmail.com>
2017-09-06 00:34:30 +0200

    Add meta data test

--- Assets/Packages/UnityFBXExporter/Editor/Nodes/Header/FbxMetaDataNodeTests.cs
+++ Assets/Packages/UnityFBXExporter/Editor/Nodes/Header/FbxMetaDataNodeTests.cs
@@ -3,15 +3,15 @@ using NUnit.Framework;
 
 namespace UnityFBXExporter
 {
-    public class FbxCreationTimeStampNodeTests
+    public class FbxMetaDataNodeTests
     {
         [Test]
         public void TestSerializeMeshModelNode()
         {
-            var sut = new FbxCreationTimeStampNode(new DateTime());
+            var sut = new FbxMetaDataNode();
 
             var actual = SampleData.ToLines(FbxAsciiWriter.SerializeGenericNode(sut, 0));
-            Assert.That(actual, Is.EqualTo(SampleData.GetTestData("Header\\CreationTimeStampNodeTest.fbx")));
+            Assert.That(actual, Is.EqualTo(SampleData.GetTestData("Header\\MetaDataTest.fbx")));
         }
     }
 
