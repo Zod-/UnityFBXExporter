394607d073fc3552ac9c96b7016201eafe92a689
Julian Hangstörfer <jhangstoerfer@gmail.com>
2017-09-06 00:40:57 +0200

    Add objects node test

---
+++ Assets/Packages/UnityFBXExporter/Editor/Nodes/TopStructure/FbxObjectsNodeTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+
+namespace UnityFBXExporter
+{
+    public class FbxObjectsNodeTests
+    {
+        [Test]
+        public void TestSerializeMeshModelNode()
+        {
+            var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            var empty = new GameObject();
+            var sut = new FbxObjectsNode(new[] { cube, empty }, new FbxConnectionsNode());
+
+            var actual = SampleData.ToLines(FbxAsciiWriter.SerializeGenericNode(sut, 0));
+            Assert.That(actual, Is.EqualTo(string.Format(SampleData.GetTestData("TopStructure\\CubeObjectTest.fbx"), FbxNode.InstanceId(cube), FbxNode.InstanceId(empty))));
+        }
+    }
+
+}
