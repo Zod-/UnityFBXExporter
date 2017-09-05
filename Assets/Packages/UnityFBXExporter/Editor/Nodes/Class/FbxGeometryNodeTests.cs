732131c1d307c1476a01c80657cc2cf9b2364375
Julian Hangstörfer <jhangstoerfer@gmail.com>
2017-09-06 00:20:11 +0200

    Add geometry node tests

---
+++ Assets/Packages/UnityFBXExporter/Editor/Nodes/Class/FbxGeometryNodeTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+
+namespace UnityFBXExporter
+{
+    public class FbxGeometryNodeTests
+    {
+        [Test]
+        public void TestSerializeMeshModelNode()
+        {
+            var mesh = GameObject.CreatePrimitive(PrimitiveType.Cube).GetMesh();
+            var fbxModelNode = new FbxGeometryNode(mesh);
+
+            var actual = SampleData.ToLines(FbxAsciiWriter.SerializeGenericNode(fbxModelNode, 0));
+            Assert.That(actual, Is.EqualTo(string.Format(SampleData.GetTestData("Class\\CubeGeometryTest.fbx"), FbxNode.InstanceId(mesh))));
+        }
+    }
+}
