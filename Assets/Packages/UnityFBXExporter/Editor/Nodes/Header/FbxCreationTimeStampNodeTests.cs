f360d6be2b1022ead8267ff0420e72e7bde7072e
Julian Hangstörfer <jhangstoerfer@gmail.com>
2017-09-06 00:32:09 +0200

    Add creation timestamp node test

---
+++ Assets/Packages/UnityFBXExporter/Editor/Nodes/Header/FbxCreationTimeStampNodeTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+namespace UnityFBXExporter
+{
+    public class FbxCreationTimeStampNodeTests
+    {
+        [Test]
+        public void TestSerializeMeshModelNode()
+        {
+            var sut = new FbxCreationTimeStampNode(new DateTime());
+
+            var actual = SampleData.ToLines(FbxAsciiWriter.SerializeGenericNode(sut, 0));
+            Assert.That(actual, Is.EqualTo(SampleData.GetTestData("Header\\CreationTimeStampNodeTest.fbx")));
+        }
+    }
+
+}
