20e5d2b9d89e2b07828b3f6ac42a6f51aafe1725
Julian Hangstörfer <jhangstoerfer@gmail.com>
2017-09-06 00:27:21 +0200

    Add scene info test

---
+++ Assets/Packages/UnityFBXExporter/Editor/Nodes/Class/FbxSceneInfoNodeTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace UnityFBXExporter
+{
+    public class FbxSceneInfoNodeTests
+    {
+        [Test]
+        public void TestSerializeMeshModelNode()
+        {
+            var sut = new FbxSceneInfoNode("TestPath");
+
+            var actual = SampleData.ToLines(FbxAsciiWriter.SerializeGenericNode(sut, 0));
+            Assert.That(actual, Is.EqualTo(SampleData.GetTestData("Class\\SceneInfoTest.fbx")));
+        }
+    }
+}
