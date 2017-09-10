c4e4c375e271b782ae08fd2192d40ea52ae86544
Julian Hangstörfer <jhangstoerfer@gmail.com>
2017-09-10 20:26:18 +0200

    Add test for threading

--- Assets/Packages/UnityFBXExporter/Editor/Nodes/TopStructure/FbxDocumentTests.cs
+++ Assets/Packages/UnityFBXExporter/Editor/Nodes/TopStructure/FbxDocumentTests.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections;
+using System.Threading;
 using NUnit.Framework;
 using UnityEngine;
+using UnityEngine.TestTools;
 using Object = UnityEngine.Object;
 
 namespace UnityFBXExporter
@@ -15,6 +18,20 @@ namespace UnityFBXExporter
             var sut = new FbxDocument(new[] { gameObject }, "TestPath", new DateTime());
 
             var actual = SampleData.ToLines(FbxAsciiWriter.SerializeDocument(sut));
+            var testDocument = SampleData.GetTestData("TopStructure\\TestDocument.fbx");
+            Assert.That(actual, Is.EqualTo(string.Format(testDocument, FbxNode.InstanceId(gameObject), FbxNode.InstanceId(cube))));
+        }
+
+        [UnityTest]
+        public IEnumerator TestFullDocumentThread()
+        {
+            var gameObject = new GameObject();
+            var cube = Object.Instantiate(GameObject.CreatePrimitive(PrimitiveType.Cube), gameObject.transform);
+            var sut = new FbxDocument(new[] { gameObject }, "TestPath", new DateTime());
+            var actual = "";
+            var thread = new Thread(() => actual = SampleData.ToLines(FbxAsciiWriter.SerializeDocument(sut)));
+            thread.Start();
+            while (thread.IsAlive) { yield return null; }
             var testDocument = SampleData.GetTestData("TopStructure\\TestDocument.fbx");
             Assert.That(actual, Is.EqualTo(string.Format(testDocument, FbxNode.InstanceId(gameObject), FbxNode.InstanceId(cube))));
         }
